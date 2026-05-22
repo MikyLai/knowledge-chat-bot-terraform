e2b33d76d15786ae8cee9df5710806b85a5d7392
mikylai <mikylai18@gmail.com>
2026-05-21 20:38:22 -0500

    restrict storage public access

--- storage.tf
+++ storage.tf
@@ -7,8 +7,8 @@ resource "azurerm_storage_account" "app" {
   account_kind             = "StorageV2" # default 值是 StorageV2，這裡明確指定以防未來變更預設
   min_tls_version          = "TLS1_2"
 
-   # 允許 public blob 存取，供 QR 圖片以 blob 層級公開讀取
-  allow_nested_items_to_be_public = true
+  # QR images served via SAS URLs; public network access required so browsers can fetch images
+  allow_nested_items_to_be_public = false
 
   # 強制 HTTPS
   https_traffic_only_enabled = true
@@ -23,5 +23,5 @@ resource "azurerm_storage_account" "app" {
 resource "azurerm_storage_container" "app" {
   name                  = "qr-code"
   storage_account_name  = azurerm_storage_account.app.name
-  container_access_type = "blob"
+  container_access_type = "private"
 }
