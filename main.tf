cfa35d09d17a31d6d20d1819513439198db78165
mikylai <mikylai18@gmail.com>
2026-05-22 12:55:37 -0500

    tf lint

--- main.tf
+++ main.tf
@@ -36,11 +36,11 @@ module "network" {
   app_service_subnet_cidr  = "10.0.1.0/24"
   db_subnet_cidr           = "10.0.2.0/24"
   address_spaces          = ["10.0.0.0/16"]
+  app_name                = var.app_name-${var.environment}
   enable_network_watcher  = true
   environment             = var.environment
   location                = var.location
   resource_group_name     = azurerm_resource_group.app.name
-  name                    = ${var.app_name}-${var.environment}
   tags                    = local.tags
 }
 
